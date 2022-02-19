8f17daff07cef2c8c576754a23f352621c0d1d36
Ivan Novak <ivan-novak@ukr.net>
2022-02-19 05:46:48 -0800

    Promotion interface

---
+++ RozetkaWebApp/Controllers/PromotionsController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RozetkaWebApp.Data;
+using RozetkaWebApp.Models;
+
+namespace RozetkaWebApp.Controllers
+{
+    public class PromotionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PromotionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Promotions
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Promotions.Include(p => p.Image);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Promotions/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var promotion = await _context.Promotions
+                .Include(p => p.Image)
+                .FirstOrDefaultAsync(m => m.PromotionId == id);
+            if (promotion == null)
+            {
+                return NotFound();
+            }
+
+            return View(promotion);
+        }
+
+        // GET: Promotions/Create
+        public IActionResult Create()
+        {
+            ViewData["ImageId"] = new SelectList(_context.Images, "ImageId", "ImageId");
+            return View();
+        }
+
+        // POST: Promotions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PromotionId,Title,Label,Description,Attributes,StartDate,EndDate,ImageId")] Promotion promotion)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var file in Request.Form.Files)
+                {
+                    var image = new Image();
+                    MemoryStream ms = new MemoryStream();
+                    file.CopyTo(ms);
+                    image.Data = ms.ToArray();
+                    image.Title = file.FileName;
+                    _context.Add(image);
+                    await _context.SaveChangesAsync();
+                    promotion.ImageId = image.ImageId;
+                    ms.Close();
+                    ms.Dispose();
+                }
+                _context.Add(promotion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ImageId"] = new SelectList(_context.Images, "ImageId", "ImageId", promotion.ImageId);
+            return View(promotion);
+        }
+
+        // GET: Promotions/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var promotion = await _context.Promotions.FindAsync(id);
+            if (promotion == null)
+            {
+                return NotFound();
+            }
+            ViewData["ImageId"] = new SelectList(_context.Images, "ImageId", "ImageId", promotion.ImageId);
+            return View(promotion);
+        }
+
+        // POST: Promotions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("PromotionId,Title,Label,Description,Attributes,StartDate,EndDate,ImageId")] Promotion promotion)
+        {
+            if (id != promotion.PromotionId)
+            {
+                return NotFound();
+            }
+            foreach (var file in Request.Form.Files)
+            {
+                MemoryStream ms = new MemoryStream();
+                file.CopyTo(ms);
+                var image = _context.Images.Find(promotion.ImageId);
+                image.Data = ms.ToArray();
+                image.Title = file.FileName;
+                _context.Update(image);
+                ms.Close();
+                ms.Dispose();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(promotion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PromotionExists(promotion.PromotionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ImageId"] = new SelectList(_context.Images, "ImageId", "ImageId", promotion.ImageId);
+            return View(promotion);
+        }
+
+        // GET: Promotions/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var promotion = await _context.Promotions
+                .Include(p => p.Image)
+                .FirstOrDefaultAsync(m => m.PromotionId == id);
+            if (promotion == null)
+            {
+                return NotFound();
+            }
+
+            return View(promotion);
+        }
+
+        // POST: Promotions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var promotion = await _context.Promotions.FindAsync(id);
+            _context.Promotions.Remove(promotion);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PromotionExists(long id)
+        {
+            return _context.Promotions.Any(e => e.PromotionId == id);
+        }
+    }
+}
